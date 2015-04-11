17a72ddb24b34ef644ff686ce2c6f09f3e7cdf41
Bruce Dawson <brucedawson@chromium.org>
2015-04-11 09:59:05 -0700

    Tweaks to metatrace.bat - more providers, trace compression, etc.

--- bin/metatrace.bat
+++ bin/metatrace.bat
@@ -1,13 +1,18 @@
-@rem Use this batch file to profile the ETW profiler.
-@rem If the save or merge steps of UIforETW or wprui
-@rem are very slow then this batch file can profile them.
-@rem Save this batch file for special occasions.
+@rem Use this batch file to profile the ETW profiler. If the save or merge
+@rem steps of UIforETW or wprui are very slow then this batch file can profile
+@rem them. Save this batch file for special occasions. This works by using
+@rem "Circular Kernel Context Logger" instead of  "NT Kernel Logger",
+@rem which makes it possible to have two kernel loggers running simultaneously.
+
+@echo Preparing to profile the ETW profiler.
 
 @setlocal
-@set kernel=-start "Circular Kernel Context Logger" -on Latency
+@set tempFile=%temp%\kernel_meta.etl
+@set kernel=-start "Circular Kernel Context Logger"
+@set kernel=%kernel% -on Latency+POWER+DISPATCHER+FILE_IO+FILE_IO_INIT
 @set kernel=%kernel% -stackwalk PROFILE+CSWITCH+READYTHREAD
 @set kernel=%kernel% -minbuffers 300 -maxbuffers 300 -buffersize 1024
-@set kernel=%kernel% -f "%temp%\circkerneldata.etl"
+@set kernel=%kernel% -f "%tempFile%"
 xperf %kernel%
 @echo Hit enter when enough data has been recorded.
 @pause
@@ -19,7 +24,8 @@ xperf -stop "Circular Kernel Context Logger"
 @rename %HVEDir%\Amcache.hve Amcache_temp.hve 2>nul
 @set RenameErrorCode=%errorlevel%
 
-xperf -merge "%temp%\circkerneldata.etl" "%temp%\metatrace.etl"
+xperf -merge "%tempFile%" "%temp%\metatrace.etl" -compress
+@del "%tempFile%"
 
 @rem Rename the file back
 @if not "%RenameErrorCode%" equ "0" goto SkipRename
