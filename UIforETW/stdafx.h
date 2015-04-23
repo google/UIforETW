eda1724b68fb1b2069a7258410316f989bd233a8
Alexander Riccio <alexander@riccio.com>
2015-04-23 02:12:26 -0400

    hacking around

--- UIforETW/stdafx.h
+++ UIforETW/stdafx.h
@@ -20,6 +20,8 @@ limitations under the License.
 #define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
 #endif
 
+#include <afxwin.h>         // MFC core and standard components //THIS NEEDS TO BE THE VERY FIRST INCLUDE!
+
 #include "targetver.h"
 
 #define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
@@ -27,7 +29,7 @@ limitations under the License.
 // turns off MFC's hiding of some common and often safely ignored warning messages
 #define _AFX_ALL_WARNINGS
 
-#include <afxwin.h>         // MFC core and standard components
+
 #include <afxext.h>         // MFC extensions
 
 
