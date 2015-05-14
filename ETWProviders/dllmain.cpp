27acaa8262359ececc1bff1d96746bdf6cce01df
Alexander Riccio <alexander@riccio.com>
2015-05-13 22:38:51 -0400

    Increased ETWProviders warning level to 4
    Fixed unreferenced parameter warning in DllMain by commenting their
    names out

--- ETWProviders/dllmain.cpp
+++ ETWProviders/dllmain.cpp
@@ -16,9 +16,9 @@ limitations under the License.
 
 #include "stdafx.h"
 
-BOOL APIENTRY DllMain( HMODULE hModule,
+BOOL APIENTRY DllMain( HMODULE /*hModule*/,
                        DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
+                       LPVOID /*lpReserved*/
 					 )
 {
 	switch (ul_reason_for_call)
