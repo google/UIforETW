6b7f99a15f4402d8c7382f5fcf60f0a7dac3f297
Bruce Dawson <brucedawson@chromium.org>
2015-06-18 14:48:33 -0700

    Fixing some minor ETWProviders problems.
    The Minimal Rebuild switch was enabled on ETWProviders debug builds
    which means the compiler warns when building them. Sloppy. Disabled.
    
    The exported ETW functions were not tagged with a calling convention,
    which is quite risky. I tagged them with __cdecl since that is what
    they were compiled with.

--- include/ETWProviders/etwprof.h
+++ include/ETWProviders/etwprof.h
@@ -39,56 +39,56 @@ const int kFlagDoubleClick = 100;
 #include <sal.h> // For _Printf_format_string_
 
 // Insert a single event to mark a point in an ETW trace.
-PLATFORM_INTERFACE void ETWMark( _In_z_ PCSTR pMessage );
+PLATFORM_INTERFACE void __cdecl ETWMark(_In_z_ PCSTR pMessage);
 // ETWWorkerMark is identical to ETWMark but goes through a different provider,
 // for different grouping.
-PLATFORM_INTERFACE void ETWWorkerMark(_In_z_ PCSTR pMessage);
+PLATFORM_INTERFACE void __cdecl ETWWorkerMark(_In_z_ PCSTR pMessage);
 
 // Insert events with one or more generic int or float data fields
-PLATFORM_INTERFACE void ETWMark1I(_In_z_ PCSTR pMessage, int data1);
-PLATFORM_INTERFACE void ETWMark2I(_In_z_ PCSTR pMessage, int data1, int data2);
-PLATFORM_INTERFACE void ETWMark1F(_In_z_ PCSTR pMessage, float data1);
-PLATFORM_INTERFACE void ETWMark2F(_In_z_ PCSTR pMessage, float data1, float data2);
+PLATFORM_INTERFACE void __cdecl ETWMark1I(_In_z_ PCSTR pMessage, int data1);
+PLATFORM_INTERFACE void __cdecl ETWMark2I(_In_z_ PCSTR pMessage, int data1, int data2);
+PLATFORM_INTERFACE void __cdecl ETWMark1F(_In_z_ PCSTR pMessage, float data1);
+PLATFORM_INTERFACE void __cdecl ETWMark2F(_In_z_ PCSTR pMessage, float data1, float data2);
 
 // _Printf_format_string_ is used by /analyze
-PLATFORM_INTERFACE void ETWMarkPrintf( _Printf_format_string_ _In_z_ PCSTR pMessage, ... );
-PLATFORM_INTERFACE void ETWWorkerMarkPrintf( _Printf_format_string_ _In_z_ PCSTR pMessage, ... );
+PLATFORM_INTERFACE void __cdecl ETWMarkPrintf(_Printf_format_string_ _In_z_ PCSTR pMessage, ...);
+PLATFORM_INTERFACE void __cdecl ETWWorkerMarkPrintf(_Printf_format_string_ _In_z_ PCSTR pMessage, ...);
 
 // Private Working Set, Proportional Set Size (shared memory charged proportionally, and total Working Set
 // counter is just a number that allows grouping together all the working set samples collected at the same time.
-PLATFORM_INTERFACE void ETWMarkWorkingSet(_In_z_ PCWSTR pProcessName, _In_z_ PCWSTR pProcess, unsigned counter, unsigned privateWS, unsigned PSS, unsigned workingSet);
+PLATFORM_INTERFACE void __cdecl ETWMarkWorkingSet(_In_z_ PCWSTR pProcessName, _In_z_ PCWSTR pProcess, unsigned counter, unsigned privateWS, unsigned PSS, unsigned workingSet);
 
 // Record powerState (charging/discharging/AC), batteryPercentage (of total capacity) and
 // discharge rate from struct BATTERY_STATUS.
-PLATFORM_INTERFACE void ETWMarkBatteryStatus(_In_z_ PCSTR powerState, float batteryPercentage, _In_z_ PCSTR rate);
+PLATFORM_INTERFACE void __cdecl ETWMarkBatteryStatus(_In_z_ PCSTR powerState, float batteryPercentage, _In_z_ PCSTR rate);
 
 // Record CPU/package frequency, power usage, and temperature. Currently Intel only.
-PLATFORM_INTERFACE void ETWMarkCPUFrequency(_In_z_ PCWSTR MSRName, double frequencyMHz);
-PLATFORM_INTERFACE void ETWMarkCPUPower(_In_z_ PCWSTR MSRName, double powerW, double energymWh);
-PLATFORM_INTERFACE void ETWMarkCPUTemp(_In_z_ PCWSTR MSRName, double tempC, double maxTempC);
+PLATFORM_INTERFACE void __cdecl ETWMarkCPUFrequency(_In_z_ PCWSTR MSRName, double frequencyMHz);
+PLATFORM_INTERFACE void __cdecl ETWMarkCPUPower(_In_z_ PCWSTR MSRName, double powerW, double energymWh);
+PLATFORM_INTERFACE void __cdecl ETWMarkCPUTemp(_In_z_ PCWSTR MSRName, double tempC, double maxTempC);
 
 // Insert a begin event to mark the start of some work. The return value is a 64-bit
 // time stamp which should be passed to the corresponding ETWEnd function.
-PLATFORM_INTERFACE int64 ETWBegin( _In_z_ PCSTR pMessage );
-PLATFORM_INTERFACE int64 ETWWorkerBegin( _In_z_ PCSTR pMessage );
+PLATFORM_INTERFACE int64 __cdecl ETWBegin(_In_z_ PCSTR pMessage);
+PLATFORM_INTERFACE int64 __cdecl ETWWorkerBegin(_In_z_ PCSTR pMessage);
 
 // Insert a paired end event to mark the end of some work.
-PLATFORM_INTERFACE int64 ETWEnd( _In_z_ PCSTR pMessage, int64 nStartTime );
-PLATFORM_INTERFACE int64 ETWWorkerEnd( _In_z_ PCSTR pMessage, int64 nStartTime );
+PLATFORM_INTERFACE int64 __cdecl ETWEnd(_In_z_ PCSTR pMessage, int64 nStartTime);
+PLATFORM_INTERFACE int64 __cdecl ETWWorkerEnd(_In_z_ PCSTR pMessage, int64 nStartTime);
 
 // Mark the start of the next render frame.
-PLATFORM_INTERFACE void ETWRenderFrameMark();
+PLATFORM_INTERFACE void __cdecl ETWRenderFrameMark();
 // Return the frame number recorded in the ETW trace -- useful for synchronizing
 // other profile information to the ETW trace.
-PLATFORM_INTERFACE int ETWGetRenderFrameNumber();
+PLATFORM_INTERFACE int __cdecl ETWGetRenderFrameNumber();
 
 // Button numbers are 0, 1, 2 for left, middle, right, with kFlagDoubleClick added
 // in for double clicks.
-PLATFORM_INTERFACE void ETWMouseDown( int nWhichButton, unsigned flags, int nX, int nY );
-PLATFORM_INTERFACE void ETWMouseUp( int nWhichButton, unsigned flags, int nX, int nY );
-PLATFORM_INTERFACE void ETWMouseMove( unsigned flags, int nX, int nY );
-PLATFORM_INTERFACE void ETWMouseWheel( unsigned flags, int zDelta, int nX, int nY );
-PLATFORM_INTERFACE void ETWKeyDown( unsigned nChar, _In_opt_z_ const char* keyName, unsigned nRepCnt, unsigned flags );
+PLATFORM_INTERFACE void __cdecl ETWMouseDown(int nWhichButton, unsigned flags, int nX, int nY);
+PLATFORM_INTERFACE void __cdecl ETWMouseUp(int nWhichButton, unsigned flags, int nX, int nY);
+PLATFORM_INTERFACE void __cdecl ETWMouseMove(unsigned flags, int nX, int nY);
+PLATFORM_INTERFACE void __cdecl ETWMouseWheel(unsigned flags, int zDelta, int nX, int nY);
+PLATFORM_INTERFACE void __cdecl ETWKeyDown(unsigned nChar, _In_opt_z_ const char* keyName, unsigned nRepCnt, unsigned flags);
 
 // This class calls the ETW Begin and End functions in order to insert a
 // pair of events to bracket some work.
