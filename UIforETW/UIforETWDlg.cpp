7af36b15bf1d361a78d555fd3e8679830d0c847f
Bruce Dawson <brucedawson@chromium.org>
2015-10-08 17:57:27 -0700

    Print a message saying what heap tracing reg key is set
    When I needed to do hardcore hacking on the heap tracing I had to look
    at the UIforETW source to remember where the registry key was. Fail. Now
    an informative (if slightly cryptic) message is printed.

--- UIforETW/UIforETWDlg.cpp
+++ UIforETW/UIforETWDlg.cpp
@@ -1471,6 +1471,7 @@ void CUIforETWDlg::SetHeapTracing(bool forceOff)
 		CreateRegistryKey(HKEY_LOCAL_MACHINE, targetKey, tracingName);
 		targetKey += L"\\" + tracingName;
 		SetRegistryDWORD(HKEY_LOCAL_MACHINE, targetKey, L"TracingFlags", tracingFlags);
+		outputPrintf(L"\"TracingFlags\" in \"HKEY_LOCAL_MACHINE\\%s\" set to %lu.\n", targetKey.c_str(), tracingFlags);
 	}
 }
 
