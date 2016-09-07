ea1129d25ba58efd03ef649829348ca553f82383
Bruce Dawson <bruce.dawson@cygnus-software.com>
2016-09-06 23:54:14 -0700

    Add source indexing to package_etw.bat
    This change uses https://github.com/Haemoglobin/GitHub-Source-Indexer.git
    to source index the UIforETW PDBs so that debugging will automatically
    download the correct source file.
    
    This change also creates an etwsymbols.zip file which will be uploaded
    with subsequent releases.

--- package_etw.bat
+++ package_etw.bat
@@ -4,14 +4,17 @@
 
 set UIforETW=%~dp0
 set destdir=%UIforETW%etwpackage
+set symboldir=%UIforETW%etwsymbols
 
 rmdir %destdir% /s/q
+rmdir %symboldir% /s/q
 @if exist %destdir%\bin\UIforETW.exe goto pleasecloseUIforETW
 @if exist %destdir%\bin\ETWProviders.dll goto pleasecloseSomething
 mkdir %destdir%\bin
 mkdir %destdir%\include
 mkdir %destdir%\lib
 mkdir %destdir%\third_party
+mkdir %symboldir%
 
 set wptredistmsi=Windows Performance Toolkit\Redistributables\WPTx64-x86_en-us.msi
 set oldwptredistmsi=Windows Performance Toolkit\OldRedistributables\WPTx64-x86_en-us.msi
@@ -79,10 +82,44 @@ xcopy %UIforETW%bin\UIforETWStatic_devrel.exe %destdir%\bin\UIforETW.exe /y
 @rem Copy the official binaries back to the local copy, for development purposes.
 xcopy /exclude:%UIforETW%excludecopy.txt %destdir%\bin\UIforETW*.exe %UIforETW%bin /y
 
-cd /d %UIforETW%
-python %UIforETW%make_zip_file.py %UIforETW%etwpackage.zip etwpackage
+xcopy %UIforETW%\bin\UIforETWStatic_devrel*.pdb %symboldir%
 
-@echo Now upload the new etwpackage.zip
+cd /d %UIforETW%
+
+@rem Source indexing as described here:
+@rem http://hamishgraham.net/post/GitHub-Source-Symbol-Indexer.aspx
+@rem The next steps assume that you have cloned https://github.com/Haemoglobin/GitHub-Source-Indexer.git
+@rem into the parent directory of the UIforETW repo.
+@rem You also have to enable execution of power-shell scripts, by running something
+@rem like this from an administrator command prompt:
+@rem powershell Set-ExecutionPolicy Unrestricted
+@rem Note that -dbgToolsPath and -verifyLocalRepo had to be added to the command line
+@rem to get the script to behave. -dbgToolsPath is tricky because the default path has
+@rem spaces and the script can't handle that, so I copy it to %temp%. God help you if
+@rem that has spaces.
+@rem verifyLocalRepo comes with its own challenges since github-sourceindexer.ps1 does
+@rem a weak job of looking for git.exe. I had to edit FindGitExe to tell it exactly where
+@rem to look - something like this:
+@rem function FindGitExe {
+@rem  	$gitexe = "c:\src\chromium\depot_tools\git-2.8.3-64_bin\cmd\git.exe"
+@rem     if (Test-Path $gitexe) {
+@rem         return $gitexe
+@rem     }
+if not exist ..\GitHub-Source-Indexer\github-sourceindexer.ps1 goto NoSourceIndexing
+mkdir %temp%\srcsrv
+xcopy /s /y "c:\Program Files (x86)\Windows Kits\10\Debuggers\x64\srcsrv" %temp%\srcsrv
+powershell ..\GitHub-Source-Indexer\github-sourceindexer.ps1 -symbolsFolder etwsymbols -userID "google" -repository UIforETW -branch "master" -sourcesRoot %UIforETW% -dbgToolsPath %temp%\srcsrv -verifyLocalRepo
+@echo Run these commands to verify what has been indexed:
+@echo %temp%\srcsrv\pdbstr -r -p:etwsymbols\UIforETWStatic_devrel.pdb -s:srcsrv
+@echo %temp%\srcsrv\pdbstr -r -p:etwsymbols\UIforETWStatic_devrel32.pdb -s:srcsrv
+:NoSourceIndexing
+
+call python make_zip_file.py etwpackage.zip etwpackage
+@echo on
+call python make_zip_file.py etwsymbols.zip etwsymbols
+@echo on
+
+@echo Now upload the new etwpackage.zip and etwsymbols.zip
 @exit /b
 
 :pleasecloseUIforETW
