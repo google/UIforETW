24049967c5cc379e34a8e1cd714d472949464300
Bruce Dawson <brucedawson@cygnus-software.com>
2019-05-16 11:57:03 -0700

    Increment version number from 1.50b to 1.51

--- UIforETW/Version.h
+++ UIforETW/Version.h
@@ -3,9 +3,9 @@
 // const float in a header file can lead to duplication of the storage
 // but I don't really care in this case. Just don't do it with a header
 // that is included hundreds of times.
-const float kCurrentVersion = 1.50f;
+const float kCurrentVersion = 1.51f;
 
 // Put a "#define VERSION_SUFFIX 'b'" line here to add a minor version
 // increment that won't trigger the new-version checks, handy for minor
 // releases that I don't want to bother users about.
-#define VERSION_SUFFIX 'b'
+//#define VERSION_SUFFIX 'b'
