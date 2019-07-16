630687676fced3919d45d636b955abe78c942e2e
Bruce Dawson <brucedawson@cygnus-software.com>
2019-07-15 23:16:11 -0700

    Add copyright notice

--- TraceProcessors/IdentifyChromeProcesses/IdentifyChromeProcesses.cs
+++ TraceProcessors/IdentifyChromeProcesses/IdentifyChromeProcesses.cs
@@ -1,4 +1,20 @@
-﻿// This script processes an ETW trace and generates a summary of the Chrome
+﻿/*
+Copyright 2019 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// This script processes an ETW trace and generates a summary of the Chrome
 // processes found within it.
 // See this blog post for details of the Trace Processor package used to
 // drive this:
