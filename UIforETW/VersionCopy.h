677d404f7508a7a791884fb5b803ce1afefca226
Bruce Dawson <brucedawson@cygnus-software.com>
2020-01-14 12:02:41 -0800

    Updating VersionCopy.h for updated version checking

--- UIforETW/VersionCopy.h
+++ UIforETW/VersionCopy.h
@@ -3,7 +3,7 @@
 // const float in a header file can lead to duplication of the storage
 // but I don't really care in this case. Just don't do it with a header
 // that is included hundreds of times.
-const float kCurrentVersion = 1.52f;
+const float kCurrentVersion = 1.53f;
 
 // Put a "#define VERSION_SUFFIX 'b'" line here to add a minor version
 // increment that won't trigger the new-version checks, handy for minor
