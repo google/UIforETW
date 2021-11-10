d107b77042aa36d0cfbaa7ea1f32090d15bd0eaa
Bruce Dawson <brucedawson@cygnus-software.com>
2021-11-10 08:47:43 -0800

    Updating VersionCopy.h for updated version checking

--- UIforETW/VersionCopy.h
+++ UIforETW/VersionCopy.h
@@ -5,7 +5,7 @@
 // that is included hundreds of times.
 // constexpr might help avoid that, but then it breaks my fragile upgrade-needed
 // detection code, so this should be left as const.
-const float kCurrentVersion = 1.55f;
+const float kCurrentVersion = 1.56f;
 
 // Put a "#define VERSION_SUFFIX 'b'" line here to add a minor version
 // increment that won't trigger the new-version checks, handy for minor
